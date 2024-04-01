8e04ceb5f405462a6da45619a45507e27aa9ca24
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-04-01 11:04:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+// properties to remember
+// mouseover - when we bring mouse over anything
+// mouseout - when remove mouse from anything
+
+const box = document.getElementsByClassName("square")
+
+for(let item of box){
+    item.addEventListener("mouseover",()=>{
+
+        for(let t of box){
+            if(t.id != item.id){
+                t.style.backgroundColor = "#6F4E37"
+
+            }
+        }
+    })
+    item.addEventListener("mouseout",()=>{
+        for(let t of box){
+            t.style.backgroundColor = "#E6E6FA"
+        }
+    })
+}
+
